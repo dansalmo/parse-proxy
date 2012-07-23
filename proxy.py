9522feace3b7ef2f096d7bfff64bc88a3e9d4c3d
Dan Salmonsen <dan@salmonsen.org>
2012-07-22 19:43:44 -0700

    Updated with final URLs

--- proxy.py
+++ proxy.py
@@ -9,7 +9,7 @@ class MainPage(webapp2.RequestHandler):
                 'email': 'dan@salmonsen.org',
                 'position': 'Inside Sales Representative',
                 'about': 'I like making technology easier for people to understand and am looking to transition into a sales role that can leverage my experience.  Excellent at communicating with technical and non-technical customers.  Experienced in a wide range of marketing and sales functions in startups to $0.5B prep-IPO and public companies',
-                'urls': ['TBD', 'TBD']
+                'urls': ['http://parse-proxy.appspot.com', 'https://github.com/dansalmo/parse-proxy']
                 }
         headers = {'Content-type': 'application/json'}
         result = urlfetch.fetch(url = url,
